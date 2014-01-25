da7b27e11b2f52e793461d9b02bc981797daaa78
rlqualls <robert@robertqualls.com>
2014-01-25 04:35:17 -0600

    Make helper consistent in issues command

--- lib/commands/issues.rb
+++ lib/commands/issues.rb
@@ -1,6 +1,7 @@
 desc "Project issues tools - sub-commands : open [user], closed [user]"
 flags :after => "Only show issues updated after a certain date"
 flags :label => "Only show issues with a certain label"
+usage "github issues [type]"
 command :issues do |command, user|
   return if !helper.project
   user ||= helper.owner
@@ -8,14 +9,14 @@ command :issues do |command, user|
   case command
   when 'open', 'closed'
     report = JSON.parse(open(@helper.list_issues_for(user, command)).read)
-    @helper.print_issues(report, options)
+    helper.print_issues(report, options)
   when 'web'
     helper.open helper.issues_page_for(user)
   when nil
     # default to open issues
     report = JSON.parse(open(@helper.list_issues_for(user, 'open')).read)
-    @helper.print_issues(report, options)
+    helper.print_issues(report, options)
   else
-    @helper.print_issues_help
+    helper.print_issues_help
   end
 end
