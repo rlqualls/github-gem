401ccee20146c310df16ce7e9bde4c7a58105810
rlqualls <robert@robertqualls.com>
2014-01-26 09:34:09 -0600

    Add intelligent default to readme command
    Without arguments, it now looks for a top-level README in the
    local repo. The command can be executed from anywhere in the repo,
    and the readme can have any extension, although markdown is assumed
    for highlighting at the moment.

--- lib/commands/commands.rb
+++ lib/commands/commands.rb
@@ -307,12 +307,19 @@ end
 desc "Output a project's README"
 usage "github readme [user]/[repo]"
 command :readme do |user, repo|
-  die "Usage: github readme [user]/[repo]" if user.nil?
-  user, repo = user.split("/") if repo.nil?
-  headers = { "Accept" =>"application/vnd.github.v3.text" }
-  data = JSON.parse(open("https://api.github.com/repos/#{user}/#{repo}/readme", headers).read)
-  die "Could not get a JSON response" unless data
-  readme_content = Base64.decode64(data["content"]).force_encoding("UTF-8")
+  if user.nil?
+    # Show top-level readme in current repo if there is one
+    # This works from anywhere inside the repo
+    readme_path = sh "git ls-files $(git rev-parse --show-toplevel)/README.*"
+    readme_content = File.open(readme_path, "r").read
+  else
+    user, repo = user.split("/") if repo.nil?
+    headers = { "Accept" =>"application/vnd.github.v3.text" }
+    data = JSON.parse(open("https://api.github.com/repos/#{user}/#{repo}/readme", headers).read)
+    die "Could not get a JSON response" unless data
+    readme_content = Base64.decode64(data["content"]).force_encoding("UTF-8")
+  end
+  # die "Usage: github readme [user]/[repo]" if user.nil?
   formatter = Rouge::Formatters::Terminal256.new
   lexer = Rouge::Lexers::Markdown.new
   formatted_content = formatter.format(lexer.lex(readme_content))
