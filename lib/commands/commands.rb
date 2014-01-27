cc99dda6bf51433a313f41569d5b42b6facbdf0f
rlqualls <robert@robertqualls.com>
2014-01-26 21:48:19 -0600

    Make view command context sensitive

--- lib/commands/commands.rb
+++ lib/commands/commands.rb
@@ -328,16 +328,40 @@ end
 
 desc "View a file in the console"
 usage "github view [user]/[repo]/[path]"
+usage "github view [user]/[repo]"
+usage "github view [user]"
 command :view do |path|
   user, repo, path = path.split("/", 3)
-  extension = path.split(".").last
-  headers = { "Accept" =>"application/vnd.github.v3.text" }
+  # If just a user
+  if repo.nil?
+    url = "https://api.github.com/users/#{user}/repos" 
+  else
+    url = "https://api.github.com/repos/#{user}/#{repo}/contents/#{path}"
+  end
   begin
-  data = JSON.parse(open("https://api.github.com/repos/#{user}/#{repo}/contents/#{path}", headers).read)
+  headers = { "Accept" =>"application/vnd.github.v3.text" }
+  data = JSON.parse(open(url, headers).read)
   rescue OpenURI::HTTPError
     die "Invalid user, repository, or file path"
   end
-  content = Base64.decode64(data["content"]).force_encoding("UTF-8")
-  formatted_content = helper.color_text(content, extension)
+  # The path was a user, repository, or directory
+  if data.is_a? Array
+    if data[0]["description"]
+      # The path was a user - list repos
+      formatted_content = data.map do |item| 
+          "#{Paint[item['name'], :blue]} -  #{item['description']}"
+      end
+    else
+      # The path was a repository - list top level files
+      formatted_content = data.map do |item| 
+          "#{item['name']}"
+      end
+    end
+  # The path was a file
+  else
+    extension = path.split(".").last
+    content = Base64.decode64(data["content"]).force_encoding("UTF-8")
+    formatted_content = helper.color_text(content, extension)
+  end
   IO.popen("less -r", "w") { |p| p.puts formatted_content }
 end
