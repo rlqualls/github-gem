e13c5384b11d44d7f04cc541980e24ea8be93ee6
rlqualls <robert@robertqualls.com>
2014-01-26 22:42:10 -0600

    Update search spec for terminal helper

--- spec/commands/command_search_spec.rb
+++ spec/commands/command_search_spec.rb
@@ -3,16 +3,40 @@ require File.expand_path("../command_helper", __FILE__)
 
 describe "github search" do
   include CommandHelper
+
+  specify "search displays on terminal" do
+    running :search, "github-gem" do
+      @helper.should_receive(:terminal_display)
+    end
+  end
   
   specify "search finds multiple results" do
     running :search, "github-gem" do
-      # @command.should_receive(:open).with("https://api.github.com/search/repositories?q=github-gem&sort=stars&order=desc")
-      stdout.should =~ "defunkt/github-gem\npjhyett/github-gem-builder\n"
+      json = StringIO.new <<-JSON
+      {
+        "total_count": 211,
+        "items": [
+          { 
+            "id": 1653,
+            "full_name": "rlqualls/github-gem",
+            "description": "command-line helper"
+          },
+          { 
+            "id": 1,
+            "full_name": "octocat/some-gem",
+            "description": "some other gem"
+          }
+        ]
+      } 
+      JSON
+
+      @command.should_receive(:open).with("https://api.github.com/search/repositories?q=github-gem&sort=stars&order=desc").and_return(json)
+      @helper.should_receive(:terminal_display)
     end
   end
 
   specify "search finds no results" do
-    running :search, "xxxxxxxxxx" do
+    running :search, "xxxxxxxxxxsdfgdfgklj4345" do
       # @command.should_receive(:open).with("https://api.github.com/search/repositories?q=xxxxxxxxxx&sort=stars&order=desc")
       stdout.should == "No results found\n"
     end
