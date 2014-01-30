cb5c17439f534441a4338bdfbd9b79379f2eae97
rlqualls <robert@robertqualls.com>
2014-01-30 09:33:54 -0600

    Move create-from-local into create --local
    There is no longer a create-from-local command. Instead, the
    create command will use the current directory if the --local flag
    is used. Also, create --local will ask for a repository name,
    defaulting to the current working directory.

--- lib/github/commands/create.rb
+++ lib/github/commands/create.rb
@@ -6,26 +6,40 @@ flags :textile => 'Create README.textile'
 flags :rdoc => 'Create README.rdoc'
 flags :rst => 'Create README.rst'
 flags :private => 'Create private repository'
+flags :local => 'Use the current working directory'
 command :create do |repo|
-  if File.directory?(repo)
-    die "Directory already exists"
-  end
-  command = "curl https://api.github.com/user/repos?access_token=#{github_token} -d '{\"name\": \"#{repo}\"'"
-  output_json = sh command
-  output = JSON.parse(output_json)
-  if output["error"]
-    die output["error"]
+  # create local 
+  if options[:local]
+    if repo.nil?
+      cwd = File.basename(Dir.getwd)
+      print "Specify a repository name <default: #{cwd}>:"
+      repo = File.basename(cwd)
+      new_name = gets.chomp
+      repo = new_name unless new_name.empty?
+    end
+    is_repo = !git("status").match(/fatal/)
+    unless is_repo
+      puts Paint['no git repository in current folder - creating one...', :bright, :green]
+      git "init"
+    end
+  # Create folder based on argument 
   else
+    die "Directory already exists" if File.directory?(repo)
     mkdir repo
     cd repo
     git "init"
-    extension = options.keys.first
-    touch extension ? "README.#{extension}" : "README"
-    git "add *"
-    git "commit -m 'Initial Commit'"
-    # give the request a bit to go through
-    sleep 2
-    git "remote add origin https://github.com/#{github_user}/#{repo}.git"
-    git_exec "push origin master"
   end
+  extension = options.keys.first
+  touch extension ? "README.#{extension}" : "README"
+  git "add *"
+  git "commit -m 'Initial Commit'"
+  puts Paint['asking GitHub.com to make the repository...', :bright, :green]
+  request = "curl https://api.github.com/user/repos?access_token=#{github_token} -d '{\"name\": \"#{repo}\"'"
+  output = sh request
+  die output["error"] if output["error"]
+  # give the request a bit to go through
+  sleep 2
+  git "remote add origin https://github.com/#{github_user}/#{repo}.git"
+  git_exec "push origin master"
+  puts Paint["done. The remote repository can be viewed with 'gh view #{github_user}/#{repo}", :bright, :green]
 end
